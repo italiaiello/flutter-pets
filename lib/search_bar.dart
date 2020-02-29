c96ab38910c3d4f32e66da01588aa6bc8dfb13af
Andrew Aiello <andrew@Andrews-MBP.modem>
2020-02-29 23:00:13 +1100

    styles search bar

--- lib/search_bar.dart
+++ lib/search_bar.dart
@@ -3,22 +3,23 @@ import 'package:flutter/material.dart';
 class SearchBar extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return Row(
-      crossAxisAlignment: CrossAxisAlignment.center,
-      mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-      children: <Widget>[
-        Icon(Icons.search),
-        SizedBox(
-          width: 200,
-          height: 50,
-          child: TextField(
-            decoration: InputDecoration(
-              border: InputBorder.none,
-              hintText: 'Search pet for adoption',
-            ),
+    return SizedBox(
+      width: 300,
+      height: 50,
+      child: TextField(
+        decoration: InputDecoration(
+          contentPadding: EdgeInsets.fromLTRB(20.0, 15.0, 20.0, 15.0),
+          prefixIcon: Icon(Icons.search),
+          hintText: "Enter Your Name",
+          border: OutlineInputBorder(
+              borderSide: BorderSide(color: Colors.blueAccent, width: 32.0),
+              borderRadius: BorderRadius.circular(25.0)),
+          focusedBorder: OutlineInputBorder(
+            borderSide: BorderSide(color: Colors.white, width: 32.0),
+            borderRadius: BorderRadius.circular(25.0),
           ),
         ),
-      ],
+      ),
     );
   }
 }
